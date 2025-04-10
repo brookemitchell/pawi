efdf7e6fdcad8ec5807fd72b9c77f004a3168a46
Brooke Mitchell <brooke-mitchell@idexx.com>
2025-04-10 22:29:47 +1200

    delete initial streamlit files

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Create the SQL connection to inventory_db as specified in your secrets file.
-conn = st.connection('inventory_db', type='sql')
-
-# Query and display the data you inserted
-pet_owners = conn.query('select * from inventory')
-st.dataframe(pet_owners)
